5a7d4ebfc8d8f9bdda7c3d6c8d6a6dc68f01fddb
Tamas <tamas.fehertoi@gmail.com>
2024-11-23 18:00:39 -0500

    create HeroSection

---
+++ src/components/LandingPage/HeroSection.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./HeroSection.css";
+
+const HeroSection = () => {
+  return (
+    <section className="hero">
+      <h1>Üdvözlünk a Café Noir világában</h1>
+      <p>Ahol a kávé és sütemények művészetté válnak.</p>
+      <button>Fedezd fel menünket</button>
+    </section>
+  );
+};
+
+export default HeroSection;
