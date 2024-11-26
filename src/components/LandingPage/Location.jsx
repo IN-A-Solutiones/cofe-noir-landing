160c75a606672dc6efaddaf4ad4350d325f48415
Tamas <tamas.fehertoi@gmail.com>
2024-11-26 14:07:44 -0500

    Location component translate & icons / resp

--- src/components/LandingPage/Location.jsx
+++ src/components/LandingPage/Location.jsx
@@ -1,18 +1,40 @@
 import React from "react";
 import "./Location.css";
+import { FaPhone } from "react-icons/fa";
+import { FaMapLocation } from "react-icons/fa6";
+import { IoIosMail } from "react-icons/io";
+import { TbClockHour3 } from "react-icons/tb";
+import { useTranslation } from "react-i18next";
 
 const Location = () => {
+  const { t } = useTranslation();
+
   return (
     <section id="location" className="location">
-      <h2>Hol találsz minket?</h2>
-      <p>Cím: 1234 Budapest, Példa utca 12.</p>
-      <div className="map">
-        <iframe
-          title="Google Map"
-          src="https://www.google.com/maps/embed?pb=..."
-          allowFullScreen=""
-          loading="lazy"
-        ></iframe>
+      <h2>{t("LOCATION.TITLE")}</h2>
+      <div className="location-content">
+        <div className="map">
+          <iframe
+            title="Google Map"
+            src="https://www.google.com/maps/embed?pb=..."
+            allowFullScreen=""
+            loading="lazy"
+          ></iframe>
+        </div>
+        <div className="contact-info">
+          <p>
+            <TbClockHour3 /> Monday - Friday: 7:00 AM - 8:00 PM
+          </p>
+          <p>
+            <FaMapLocation /> 1234 New York, NE 12.
+          </p>
+          <p>
+            <FaPhone /> +1 123 45678
+          </p>
+          <p>
+            <IoIosMail /> info@coffe.com
+          </p>
+        </div>
       </div>
     </section>
   );
