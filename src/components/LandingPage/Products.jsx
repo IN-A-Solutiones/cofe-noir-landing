ecd4aef3c9f7148b9bb2bba3ad1352d94486651f
Tamas <tamas.fehertoi@gmail.com>
2024-11-26 12:21:49 -0500

    add content to Product section

--- src/components/LandingPage/Products.jsx
+++ src/components/LandingPage/Products.jsx
@@ -1,16 +1,33 @@
 import React from "react";
 import "./Products.css";
-import Product1 from "../../assets/pictures/LandingPage/product1.webp";
+import Product1 from "../../assets/pictures/LandingPage/BerryCheesecakeImage.webp";
+import Product2 from "../../assets/pictures/LandingPage/LatteArtImage.webp";
+import Product3 from "../../assets/pictures/LandingPage/GourmetSandwichImage.png";
 
 const Products = () => {
   const products = [
     {
       id: 1,
-      name: "Házi málnatorta",
       image: Product1,
+      name: "Berry Cheesecake",
+      summary: "Indulge in our creamy cheesecake topped with fresh berries.",
     },
-    { id: 2, name: "Barista Latte Art", image: "/images/product2.jpg" },
-    { id: 3, name: "Epres sajttorta", image: "/images/product3.jpg" },
+
+    {
+      id: 2,
+      name: "Barista Latte Art",
+      image: Product2,
+      summary:
+        "A classic latte with a touch of love, perfect for any coffee lover.",
+    },
+
+    {
+      id: 3,
+      name: "Gourmet Sandwich",
+      image: Product3,
+      summary:
+        "Crafted with the freshest ingredients, our sandwiches are a taste delight.",
+    },
   ];
   return (
     <section id="menu" className="products">
@@ -20,6 +37,7 @@ const Products = () => {
           <div key={product.id} className="product-card">
             <img src={product.image} alt={product.name} />
             <h3>{product.name}</h3>
+            <p>{product.summary}</p>
           </div>
         ))}
       </div>
