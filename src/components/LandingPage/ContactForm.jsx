bed7b8b19aec87dce80be00078f7c2e3b7db27bb
Tamas <tamas.fehertoi@gmail.com>
2024-11-26 14:36:43 -0500

    Footer & Contact  translate

--- src/components/LandingPage/ContactForm.jsx
+++ src/components/LandingPage/ContactForm.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import "./ContactForm.css";
+import { useTranslation } from "react-i18next";
 
 const ContactForm = () => {
+  const { t } = useTranslation();
+
   return (
     <section id="contact" className="contact">
-      <h2>Kapcsolat</h2>
+      <h2>{t("CONTACT.TITLE")}</h2>
       <form>
-        <input type="text" placeholder="Név" required />
-        <input type="email" placeholder="Email" required />
-        <textarea placeholder="Üzenet" required></textarea>
-        <button type="submit">Küldés</button>
+        <input type="text" placeholder={t("CONTACT.INPUT1")} required />
+        <input type="email" placeholder={t("CONTACT.INPUT2")} required />
+        <textarea placeholder={t("CONTACT.INPUT3")} required></textarea>
+        <button type="submit">{t("CONTACT.BUTTON")}</button>
       </form>
     </section>
   );
--- src/components/LandingPage/Footer.jsx
+++ src/components/LandingPage/Footer.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import "./Footer.css";
+import { FaFacebook, FaInstagram, FaTiktok } from "react-icons/fa";
+import { useTranslation } from "react-i18next";
 
 const Footer = () => {
+  const { t } = useTranslation();
+
   return (
     <footer className="footer">
-      <p>&copy; 2024 Café Noir. Minden jog fenntartva.</p>
+      <div className="footer-container">
+        <FaInstagram className="footer-icon" />
+        <FaFacebook className="footer-icon" />
+        <FaTiktok className="footer-icon" />
+      </div>
+      <p>&copy; 2024 Café Noir. {t("FOOTER.COPYWRITE")}</p>
     </footer>
   );
 };
